76ab391a6fc479597b432f386fc5806809ae9bc5
chenyao <444722407@qq.com>
2022-05-24 17:32:17 +0800

    frist

---
+++ layouts/blog.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>哈哈哈，layouts设置了布局</h1>
+    <nuxt />
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>111111111</h1>
+        <nuxt-child/>
+    </div>
+</template>
+
+<script>
+export default{
+     layout: 'blog'
+}
+</script>
